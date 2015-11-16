0db1e165e05c959dee7be4dec5e1a23705736760
Christoph Gockel <github@christophgockel.de>
2015-11-16 18:08:55 +0000

    Separates i/o from the worker routines

--- goony.go
+++ goony.go
@@ -8,30 +8,37 @@ import (
 	"os"
 )
 
-const ROUTINES = 10
-
 func main() {
 	options, _ := config.ParseArguments(os.Args[1:]...)
 
 	linesChannel := make(chan string)
 	done := make(chan bool)
+	resultsChannel := make(chan request.Result, 10000)
 
 	file, _ := os.Open(options.File)
 	go files.StreamContent(file, linesChannel)
 
-	for i := 0; i < ROUTINES; i++ {
+	for i := 0; i < options.NumberOfRoutines; i++ {
 		go func() {
 			for line := range linesChannel {
-				result := request.Get(line, options.Host, http.DefaultClient)
-
-				files.Print(result, os.Stdout)
+				resultsChannel <- request.Get(line, options.Host, http.DefaultClient)
 			}
 
 			done <- true
 		}()
 	}
 
-	for i := 0; i < ROUTINES; i++ {
+	go func() {
+		for result := range resultsChannel {
+			files.Print(result, os.Stdout)
+		}
+		done <- true
+	}()
+
+	for i := 0; i < options.NumberOfRoutines; i++ {
 		<-done
 	}
+
+	close(resultsChannel)
+	<-done
 }
